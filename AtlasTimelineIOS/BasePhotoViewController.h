1530ca9072a06326e5a8619cb0a196b24be80830
hongliuli <hongliuli@yahoo.com>
2013-12-13 22:46:51 -0500

    add to zoom photo

---
+++ AtlasTimelineIOS/BasePhotoViewController.h
@@ -0,0 +1,21 @@
+//
+//  BasePhotoViewController.h
+//  PhotoScroller
+//
+//  Created by Stephanie Sharp on 19/06/13.
+//
+
+#import <UIKit/UIKit.h>
+@class ATEventEditorTableController;
+
+@interface BasePhotoViewController : UIViewController <UIPageViewControllerDataSource>
+
+@property (nonatomic, strong) UIPageViewController * pageViewController;
+@property (strong, nonatomic) IBOutlet UIPageControl *pageControl;
+@property (strong, nonatomic) IBOutlet UIToolbar *toolbar;
+@property (strong, nonatomic) NSMutableArray* photoList;
+
+@property (weak, nonatomic) ATEventEditorTableController* eventEditor;
+@property int currentIndex;
+
+@end
