4265c7302d05d47b67c9f8ec0fe311636c3053fc
hongliuli <hongliuli@yahoo.com>
2013-12-24 14:31:09 -0500

    add magnifier to time wheel's centered date

--- AtlasTimelineIOS/ATTimeZoomLine.h
+++ AtlasTimelineIOS/ATTimeZoomLine.h
@@ -15,7 +15,7 @@
 @property float scaleLenForDisplay;
 
 - (void)changeScaleText:(NSString*)text; //called in mapview controller
-- (void)changeDateText:(NSString*)text; //called in timeWindowNew
+- (void)changeDateText:(NSString*)yearText :(NSString*)monthText; //called in timeWindowNew
 - (void)showHideScaleText:(BOOL)showFlag; //called in scroll window when start scroll and stop scroll
 - (void)showHideInAnimation;
 - (void)changeScaleLabelsDateFormat:(NSDate*)startDay :(NSDate*)endDay;
