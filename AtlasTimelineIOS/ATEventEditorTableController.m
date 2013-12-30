825c70306b228788e68c0cbafd134840b79041f2
hongliuli <hongliuli@yahoo.com>
2013-12-29 23:13:25 -0500

    allow share the first 10 photos, instead of only 1. cleaned up share buttons after change to share more than one photos

--- AtlasTimelineIOS/ATEventEditorTableController.m
+++ AtlasTimelineIOS/ATEventEditorTableController.m
@@ -161,10 +161,16 @@ forRowAtIndexPath: (NSIndexPath*)indexPath
         [customView addSubview:label];
         
         UIButton *shareButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        shareButton.frame = CGRectMake(200, 0, 100, 30);
-        [shareButton setImage:[UIImage imageNamed:@"share-icons.png"] forState:UIControlStateNormal];
+        shareButton.frame = CGRectMake(150, 0, 100, 30);
+        [shareButton setImage:[UIImage imageNamed:@"share.png"] forState:UIControlStateNormal];
         [shareButton addTarget:self action:@selector(shareButtonAction:) forControlEvents:UIControlEventTouchUpInside];
         [customView addSubview:shareButton];
+        
+        UILabel* label2 = [[UILabel alloc] initWithFrame:CGRectMake(220, 0, 100, 40)];
+        label2.font = [UIFont fontWithName:@"Helvetica" size:9];
+        label2.backgroundColor = [UIColor clearColor];
+        label2.text = @"(The 1st 10 photos)";
+        [customView addSubview:label2];
     }
     return customView;
 }
@@ -275,24 +281,28 @@ forRowAtIndexPath: (NSIndexPath*)indexPath
                                                                delegate:nil cancelButtonTitle:@"Dismiss" otherButtonTitles:nil];
             [alertView show];
         }
-        NSArray *activityItems;
         
         APActivityProvider *ActivityProvider = [[APActivityProvider alloc] init];
         ActivityProvider.eventEditor = self;
-
-        int selectedIndex = self.photoScrollView.selectedAsShareIndex;
+        NSMutableArray *activityItems = [[NSMutableArray alloc] init];
+    
+        //int selectedIndex = self.photoScrollView.selectedAsShareIndex;
         if (self.photoScrollView.photoList != nil && [self.photoScrollView.photoList count] > 0)
         {
-            NSString* photoForShareName = self.photoScrollView.photoList[selectedIndex];
-            NSString* photoFullPath = [[[ATHelper getPhotoDocummentoryPath] stringByAppendingPathComponent:self.eventId] stringByAppendingPathComponent:photoForShareName];
-            if ([photoForShareName hasPrefix:NEW_NOT_SAVED_FILE_PREFIX]) //in case selected a unsaved image for share
-                photoFullPath = [[ATHelper getNewUnsavedEventPhotoPath] stringByAppendingPathComponent:photoForShareName];
-            UIImage* img = [UIImage imageWithContentsOfFile:photoFullPath];
-            activityItems = @[img, ActivityProvider];
-        } else {
-            activityItems = @[ActivityProvider];
-        }
-    
+            for (int selectedIndex = 0; selectedIndex < [self.photoScrollView.photoList count] ; selectedIndex++)
+            {
+                if (selectedIndex == 10)
+                    break; //only attache the first 10 photos because facebook will not show
+                NSString* photoForShareName = self.photoScrollView.photoList[selectedIndex];
+                NSString* photoFullPath = [[[ATHelper getPhotoDocummentoryPath] stringByAppendingPathComponent:self.eventId] stringByAppendingPathComponent:photoForShareName];
+                if ([photoForShareName hasPrefix:NEW_NOT_SAVED_FILE_PREFIX]) //in case selected a unsaved image for share
+                    photoFullPath = [[ATHelper getNewUnsavedEventPhotoPath] stringByAppendingPathComponent:photoForShareName];
+                UIImage* img = [UIImage imageWithContentsOfFile:photoFullPath];
+                [activityItems addObject:img];
+            }
+        } 
+        [activityItems addObject:ActivityProvider];
+        
         UIActivityViewController *activityController =
         [[UIActivityViewController alloc]
          initWithActivityItems:activityItems
