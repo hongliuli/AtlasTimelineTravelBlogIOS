6785d5eba82aa929dee498269ab72badab2a6352
hongliuli <hongliuli@yahoo.com>
2014-01-20 23:06:16 -0500

    make currently selected annotation with a image

---
+++ AtlasTimelineIOS/ATAnnotationFocused.h
@@ -0,0 +1,13 @@
+//
+//  ATAnnotationFocused.h
+//  AtlasTimelineIOS
+//
+//  Created by Hong on 1/15/14.
+//  Copyright (c) 2014 hong. All rights reserved.
+//
+
+#import "ATEventAnnotation.h"
+
+@interface ATAnnotationFocused : ATEventAnnotation
+
+@end
---
+++ AtlasTimelineIOS/ATAnnotationFocused.m
@@ -0,0 +1,13 @@
+//
+//  ATAnnotationFocused.m
+//  AtlasTimelineIOS
+//
+//  Created by Hong on 1/15/14.
+//  Copyright (c) 2014 hong. All rights reserved.
+//
+
+#import "ATAnnotationFocused.h"
+
+@implementation ATAnnotationFocused
+
+@end
